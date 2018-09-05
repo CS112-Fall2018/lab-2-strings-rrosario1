29022ff7bfd212e8434b54a12e2863c3e68ab47c
Ramiro Rosario <rrosario@dons.usfca.edu>
2018-09-05 22:14:36 +0100

    commiting the StringFun java file.

---
+++ StringFun.java
@@ -0,0 +1,26 @@
+public class StringFun {
+	public static void main(String[] args) {
+
+		String s = "This is a string.";
+		String otherString = "This is another string.";
+		String subString = "Another string.";
+
+		System.out.println(s.length());
+		System.out.println(s.equals(otherString));
+		System.out.println(s.substring(1, 4));
+		System.out.println(s.trim());
+		System.out.println(s.indexOf(subString));
+		System.out.println(s.lastIndexOf(subString));
+		System.out.println(s.charAt(3));
+		System.out.println(s.toLowerCase());
+		System.out.println(s.replace('i', 'x'));
+
+		/**
+		* toLowerCase changes the case of all characters to lowercase
+		* replace (s.replace(oldchar, newchar)) replaces a character (oldchar) with a new character (newchar)
+		*
+		*/
+		
+
+	}
+}
